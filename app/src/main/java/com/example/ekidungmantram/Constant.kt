16147fc5b2529fe937b1ed1f50e77ff7c66d2172
vigyanmelati <gyanixc11@gmail.com>
2023-02-23 23:24:54 +0800

    ubah base_url

--- app/src/main/java/com/example/ekidungmantram/Constant.kt
+++ app/src/main/java/com/example/ekidungmantram/Constant.kt
@@ -2,13 +2,13 @@ package com.example.ekidungmantram
 
 object Constant  {
 //    const val BASE_URL: String      = "https://api-wahyu.devmptr.com/api/"
-    const val BASE_URL: String      = "https://7698-2001-448a-5065-19cd-40f1-94ba-a7c2-e287.ap.ngrok.io/api/"
-    const val IMAGE_URL: String     = "https://7698-2001-448a-5065-19cd-40f1-94ba-a7c2-e287.ap.ngrok.io/gambarku/"
-    const val AUDIO_URL: String     = "https://7698-2001-448a-5065-19cd-40f1-94ba-a7c2-e287.ap.ngrok.io/audioku/"
-    const val PDF_URL: String     = "https://7698-2001-448a-5065-19cd-40f1-94ba-a7c2-e287.ap.ngrok.io/fileAhli/"
+    const val BASE_URL: String      = "http://githa.vigyanmelati.my.id/api/"
+    const val IMAGE_URL: String     = "http://githa.vigyanmelati.my.id/gambarku/"
+    const val AUDIO_URL: String     = "http://githa.vigyanmelati.my.id/audioku/"
+    const val PDF_URL: String     = "http://githa.vigyanmelati.my.id/fileAhli/"
 //    const val IMAGE_URL: String     = "https://api-wahyu.devmptr.com/gambarku/"
     const val API_KEY: String       = "AIzaSyDXHvS9eiDSYJGwE3XEcW8S1O-T1ptVlag"
 //    const val URL: String           = "http://api-wahyu.devmptr.com/"
-    const val URL: String           = "https://7698-2001-448a-5065-19cd-40f1-94ba-a7c2-e287.ap.ngrok.io/"
+    const val URL: String           = "http://githa.vigyanmelati.my.id/"
     const val DATABASE_VERSION: Int = 1
 }
